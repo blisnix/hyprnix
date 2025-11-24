90287e7823a19741c3ae8641661906d25b6372a5
Don Williams <don.e.williams@gmail.com>
2025-11-24 12:06:09 -0500

    Moved kitty-bg desktop to home mgr

--- config/kitty.nix
+++ config/kitty.nix
@@ -264,12 +264,6 @@
               --override "background_opacity=1.0"
             )
 
-            # Create desktop file for kitty-bg
-            DESKTOP_DIR="$HOME/.local/share/applications"
-            DESKTOP_FILE="$DESKTOP_DIR/kitty-bg.desktop"
-            mkdir -p "$DESKTOP_DIR"
-            printf '%s\n' '[Desktop Entry]' 'Type=Application' 'Name=Kitty with Random Wallpaper' 'Comment=Launch Kitty terminal with a random wallpaper' 'Exec=kitty-bg' 'Icon=utilities-terminal' 'Terminal=false' 'Categories=System;Utility;TerminalEmulator;' > "$DESKTOP_FILE"
-
             # Launch kitty (background by default) with background image overrides
             if (( LAUNCH )); then
               if (( FOREGROUND )); then
@@ -280,4 +274,14 @@
             fi
     '')
   ];
+
+  # Desktop file for kitty-bg so it shows up in application launchers
+  xdg.desktopEntries.kitty-bg = {
+    name = "Kitty with Random Wallpaper";
+    comment = "Launch Kitty terminal with a random wallpaper";
+    exec = "kitty-bg";
+    icon = "utilities-terminal";
+    terminal = false;
+    categories = [ "System" "Utility" "TerminalEmulator" ];
+  };
 }
